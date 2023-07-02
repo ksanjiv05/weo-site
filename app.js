24bf8d9fafbe0686b9f8c107551a64f1de668c22
ksanjiv05 <kumarsanjiv0005@gamil.com>
2023-07-02 18:03:22 +0530

    app configured

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ app.get('/',(req,res) => {
     res.sendFile(__dirname + '/public/index.html');
 })
 
-app.listen(4000, () => {
-    console.log('Server is running on port 4000');
-})
+// app.listen(4000, () => {
+//     console.log('Server is running on port 4000');
+// })
+
+module.exports = app;
